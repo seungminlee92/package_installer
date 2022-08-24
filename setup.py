ad9417ff932de7f3e6e989ee8183573f188fbcc0
IG <seungminlee92@gmail.com>
2022-08-24 13:21:05 +0900

    ver1 upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pkg-installer", 
+    version="1.0.0.0", 
+    author="SML", 
+    author_email="seungminlee92@kakao.com", 
+    description="git modules installer",
+    license="SEUNGMINLEE", 
+    packages=find_packages(), 
+    data_files=[], 
+    install_requires=[
+        "importlib_metadata"
+    ]
+)

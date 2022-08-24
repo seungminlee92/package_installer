fc23cd2e3d12beb2d46faa86acc96f6ca9e440b1
IG <seungminlee92@gmail.com>
2022-08-24 14:03:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name="pkg-installer", 
     version="1.0.1", 
+    description="python package installer", 
     author="SML", 
     author_email="seungminlee92@kakao.com", 
-    description="git modules installer",
-    license="SEUNGMINLEE", 
+    url="https://github.com/seungminlee92/package_installer", 
+    license="MIT", 
     packages=find_packages(), 
     data_files=[], 
     install_requires=[
